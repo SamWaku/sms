2173b094791abee2064d8245bf1f7811f02a391e
Samuel Wakumelo <samuelwaku1st@gmail.com>
2024-07-18 14:32:53 +0200

    email

--- SMS/settings.py
+++ SMS/settings.py
@@ -133,12 +133,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 # settings.py
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'chitailapious1@gmail.com'
-EMAIL_HOST_PASSWORD = 'Clever365@!!'
+EMAIL_HOST_USER = 'samuelwaku1st@gmail.com'
+EMAIL_HOST_PASSWORD = 'bvqj qrvb sluf aeyl'
 
 
 
