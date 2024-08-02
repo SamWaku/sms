29a67a9ae9fbe078606f14149a6714a8a12f7dcf
Samuel Wakumelo <samuelwaku1st@gmail.com>
2024-08-02 16:26:03 +0200

    views5

--- students/urls.py
+++ students/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import groups, get_fields, get_students
+from .views import groups, get_fields, get_students, add_student_courses
 
 
 
@@ -21,5 +21,7 @@ urlpatterns = [
     path('get_students/', views.get_students, name='get_students'),
     path('get-students-group/', views.get_students_group, name='get_students_group'),
 
+    path('add-course/', views.add_student_courses, name='add_student_courses'),
     path('add-course/', views.add_student_courses, name='add_student_courses')
+
 ]
